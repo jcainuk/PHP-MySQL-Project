1b4336cb2c4787c89c04e5866b2e1f114c796ef9
Jonathan Cain <jcainuk@gmail.com>
2024-05-28 14:18:30 +0100

    Renders the hard-coded articles array into html

--- index.php
+++ index.php
@@ -13,3 +13,33 @@ $articles = [
     "content" => "You must read this now, it's essential reading!"
   ]
 ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Blog</title>
+</head>
+
+<body>
+  <header>
+    <h1>My Blog</h1>
+  </header>
+  <main>
+    <ul>
+      <?php foreach ($articles as $article) : ?>
+        <li>
+          <article>
+            <h2><?= $article['title']; ?></h2>
+            <p><?= $article['content']; ?></p>
+          </article>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </main>
+
+</body>
+
+</html>
