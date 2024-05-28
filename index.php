b9c72c4e2c18893e30e0be7261067dbb58fc21dc
Jonathan Cain <jcainuk@gmail.com>
2024-05-28 15:35:14 +0100

    create an sql query and send it to the database using the mysqli_query() function and passing it the connection variable and the sql query variable

--- index.php
+++ index.php
@@ -14,20 +14,33 @@ $articles = [
   ]
 ];
 
+// Credentials
 $db_host = "localhost";
 $db_name = "cms";
 $db_user = "cms_www";
 $db_pass = "CpGIIO(bYdn248HU";
 
+// Save connection to database
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
+// Check if not connected
 if (mysqli_connect_error()) {
   echo mysqli_connect_error();
   exit;
 }
 
+// Print a message if connected successfully
 echo "Connected successfully.";
 
+
+// SQL query 
+$sql = "SELECT *
+        FROM article
+        ORDER BY published_at";
+
+
+// Send SQL query to database
+$results = mysqli_query($conn, $sql);
 
 ?>
 <!DOCTYPE html>
