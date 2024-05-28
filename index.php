59a0c2fd48b7746d08406712c509468f3355a4d7
Jonathan Cain <jcainuk@gmail.com>
2024-05-28 15:48:55 +0100

    deletes the hard-coded array

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-$articles = [
-  [
-    "title" => "First post",
-    "content" => "This is the first of many posts!"
-  ],
-  [
-    "title" => "Another post",
-    "content" => "Yet another fascinating post..."
-  ],
-  [
-    "title" => "Read this!",
-    "content" => "You must read this now, it's essential reading!"
-  ]
-];
 
 // Credentials
 $db_host = "localhost";
