685f796242b5345d85e5c86551ade6c66a8e15c4
Jonathan Cain <jcainuk@gmail.com>
2024-05-28 17:24:36 +0100

    extrapolates the database connection into a seprate file and requires that file in article.php and index.php

--- article.php
+++ article.php
@@ -1,23 +1,6 @@
 <?php
 
-// Credentials
-$db_host = "localhost";
-$db_name = "cms";
-$db_user = "cms_www";
-$db_pass = "CpGIIO(bYdn248HU";
-
-// Save connection to database
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-// Check if not connected
-if (mysqli_connect_error()) {
-  echo mysqli_connect_error();
-  exit;
-}
-
-// Print a message if connected successfully
-//echo "Connected successfully.";
-
+require 'database.php';
 
 // check the id is numeric before passing to sql query
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
