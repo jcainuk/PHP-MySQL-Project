c6598d96dd58a9fe8daa92ecbdf79ca05549c83a
Jonathan Cain <jcainuk@gmail.com>
2024-05-29 15:20:21 +0100

    start to tackle working with html and php in forms

---
+++ form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="process_form.php">
+    <input name="search">
+    <button>Send</button>
+  </form>
+
+</body>
+
+</html>
