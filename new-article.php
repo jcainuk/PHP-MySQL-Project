520dda5e189b82970c1b6dc129bef582685ddbc0
Jonathan Cain <jcainuk@gmail.com>
2024-05-29 17:28:48 +0100

    adds my basic form template

--- new-article.php
+++ new-article.php
@@ -2,6 +2,24 @@
 
 <h2>New Article</h2>
 
-<form action=""></form>
+<form method="post">
+
+  <div>
+    <label for="title">Title</label>
+    <input name="title" id="title" placeholder="Article title">
+  </div>
+
+  <div>
+    <label for="content">Content</label>
+    <textarea name="content" id="content" placeholder="Article content" rows="4" cols="40"></textarea>
+  </div>
+
+  <div>
+    <label for="published_at">Publication date and time</label>
+    <input type="datetime-local" name="published_at" id="published_at">
+  </div>
+
+  <button>Add</button>
+</form>
 
 <?php require 'includes/footer.php'; ?>
