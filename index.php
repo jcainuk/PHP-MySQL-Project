aaa3497ec34ca1c5b2736e3a935ea1cf6f6d2f70
Jonathan Cain <jcainuk@gmail.com>
2024-05-30 20:35:35 +0100

    adds a link from index.php to add a new article

--- index.php
+++ index.php
@@ -25,6 +25,8 @@ if ($results === false) {
 ?>
 <?php require 'includes/header.php'; ?>
 
+<a href="new-article.php">New article</a>
+
 <?php if (empty($articles)) : ?>
   <p>No articles found.</p>
 <?php else : ?>
