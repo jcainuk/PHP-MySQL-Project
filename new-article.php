05ebfa4226152229e9468035a0814724d855b725
Jonathan Cain <jcainuk@gmail.com>
2024-06-03 14:18:38 +0100

    extrapolates the code for the form into its own includes file

--- new-article.php
+++ new-article.php
@@ -81,37 +81,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <h2>New Article</h2>
 
-<?php if (!empty($errors)) : ?>
-
-  <ul>
-    <?php foreach ($errors as $error) : ?>
-
-      <li><?= $error ?></li>
-
-    <?php endforeach; ?>
-  </ul>
-
-
-<?php endif; ?>
-
-<form method="post">
-
-  <div>
-    <label for="title">Title</label>
-    <input name="title" id="title" placeholder="Article title" value="<?= htmlspecialchars($title); ?>">
-  </div>
-
-  <div>
-    <label for="content">Content</label>
-    <textarea name="content" id="content" placeholder="Article content" rows="4" cols="40"><?= htmlspecialchars($content); ?></textarea>
-  </div>
-
-  <div>
-    <label for="published_at">Publication date and time</label>
-    <input type="datetime-local" name="published_at" id="published_at" value="<?= htmlspecialchars($published_at); ?>">
-  </div>
-
-  <button>Add</button>
-</form>
+<?php require 'includes/article-form.php'; ?>
 
 <?php require 'includes/footer.php'; ?>
