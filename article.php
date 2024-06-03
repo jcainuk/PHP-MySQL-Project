a161144a96a7c8c0ca6cbe1452c8396748f221d5
Jonathan Cain <jcainuk@gmail.com>
2024-06-03 17:04:37 +0100

    adds a link to edit the article on the individual article page

--- article.php
+++ article.php
@@ -26,6 +26,7 @@ if (isset($_GET['id'])) {
     <h2><?= htmlspecialchars($article['title']); ?></h2>
     <p><?= htmlspecialchars($article['content']); ?></p>
   </article>
+  <a href="edit-article.php?id=<?= $article['id']; ?>">Edit</a>
 
 <?php endif; ?>
 
