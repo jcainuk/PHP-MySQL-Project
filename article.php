baa4e6e67c57d37a7d3060fe04d521ddb8c28c3c
Jonathan Cain <jcainuk@gmail.com>
2024-06-03 14:08:11 +0100

    creates a function to get a single article and extrapolates the logic for it to an new file

--- article.php
+++ article.php
@@ -1,28 +1,13 @@
 <?php
 
 require 'includes/database.php';
+require 'includes/article.php';
 
 $conn = getDb();
 
 // check the id is numeric before passing to sql query
-if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-  // SQL query 
-  $sql = "SELECT *
-        FROM article
-        WHERE id = " . $_GET['id'];
-
-
-  // Send SQL query to database
-  $results = mysqli_query($conn, $sql);
-
-  // If there's a problem with the results show the error
-  if ($results === false) {
-    echo mysqli_error($conn);
-
-    // else fetch all the results
-  } else {
-    $article = mysqli_fetch_assoc($results);
-  }
+if (isset($_GET['id'])) {
+  $article = getArticle($conn, $_GET['id']);
 } else {
   $article = null;
 }
