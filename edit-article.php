a50ca57bc1dcd8527ac2a04db58d39f6f96d62b4
Jonathan Cain <jcainuk@gmail.com>
2024-06-03 14:38:39 +0100

    adds a form for editing an existing article

--- edit-article.php
+++ edit-article.php
@@ -5,11 +5,22 @@ require 'includes/article.php';
 
 $conn = getDb();
 
-
 if (isset($_GET['id'])) {
   $article = getArticle($conn, $_GET['id']);
+
+  $title = $article['title'];
+  $content = $article['content'];
+  $published_at = $article['published_at'];
 } else {
   $article = null;
 }
 
-var_dump($article);
+?>
+
+<?php require 'includes/header.php'; ?>
+
+<h2>Edit Article</h2>
+
+<?php require 'includes/article-form.php'; ?>
+
+<?php require 'includes/footer.php'; ?>
