5341335bb55c01dd30e87bc8a42eba6bea225de3
Jonathan Cain <jcainuk@gmail.com>
2024-06-04 18:58:40 +0100

    adds a login form and processes the user's credentials (I'm testing it with just a hard-coded value for now to see how this works)

--- login.php
+++ login.php
@@ -2,4 +2,31 @@
 
 session_start();
 
-$_SESSION['is_logged_in'] = true;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if ($_POST['username'] == 'jonathan' && $_POST['password'] == 'secret') {
+    die('login correct');
+  } else {
+    die('login incorrect');
+  }
+}
+
+?>
+
+<?php require 'includes/header.php'; ?>
+
+<h2>Login</h2>
+
+<form method="post">
+  <div>
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+  </div>
+  <div>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+  </div>
+
+  <button>Log in</button>
+</form>
+
+<?php require 'includes/footer.php'; ?>
