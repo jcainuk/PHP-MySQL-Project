a89a0e94f2ec3ececc253cea1a6f320eb4e7b85d
Jonathan Cain <jcainuk@gmail.com>
2024-06-04 19:31:07 +0100

    makes sure user cannot submit an article unless they are logged in

--- new-article.php
+++ new-article.php
@@ -2,6 +2,13 @@
 require 'includes/database.php';
 require 'includes/article.php';
 require 'includes/url.php';
+require 'includes/auth.php';
+
+session_start();
+
+if (!isLoggedin()) {
+  die('unauthorised');
+}
 
 $errors = [];
 $title = '';
