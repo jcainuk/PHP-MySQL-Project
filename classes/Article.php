7459a27c2df7b8b24edbab5e4e1b81f143c4270c
Jonathan Cain <jcainuk@gmail.com>
2024-06-08 18:00:05 +0100

    uses a class based method to get an article by id

--- classes/Article.php
+++ classes/Article.php
@@ -25,4 +25,25 @@ class Article
 
     return  $results->fetchAll(PDO::FETCH_ASSOC);
   }
+
+  /**
+   * Get the article record base on the ID
+   * @param  object $conn Connection to the database.
+   * @param  integer $id the article ID.
+   * @param string $columns Optional list of columns for the select, defaults to *
+   * @return mixed An associative array containing the article with that ID, or null if not found.
+   */
+  public static function getById($conn, $id, $columns = '*')
+  {
+    $sql = "SELECT $columns FROM article WHERE id = :id";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+    if ($stmt->execute()) {
+
+      return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+  }
 }
