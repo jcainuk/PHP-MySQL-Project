1df0ed16f79c73ab5af61f3b954ca8b0132a5518
Jonathan Cain <jcainuk@gmail.com>
2024-06-10 14:41:16 +0100

    updates the authenticate method to check the credentials from the database

--- classes/User.php
+++ classes/User.php
@@ -26,13 +26,31 @@ class User
   /**
    * Authenticate a user by username and password
    * 
+   * @param object $conn Connection to the database
    * @param string $username Username
    * @param string $password Password
    * 
-   * @return boolean True if the credentials are correct, false otherwise
+   * @return boolean True if the credentials are correct, null otherwise
    */
-  public static function authenticate($username, $password)
+  public static function authenticate($conn, $username, $password)
   {
-    return $username === 'jonathan' && $password === 'secret';
+    $sql = "SELECT *
+            FROM user
+            WHERE username = :username";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+
+    $stmt->setFetchMode(PDO::FETCH_CLASS, 'User');
+
+    $stmt->execute();
+
+    $user = $stmt->fetch();
+
+    if ($user) {
+      if ($user->password === $password) {
+        return true;
+      }
+    }
   }
 }
