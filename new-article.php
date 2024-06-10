09cc466aa35c46151e1dcc9e2ffb01894fa5577d
Jonathan Cain <jcainuk@gmail.com>
2024-06-10 16:06:52 +0100

    refactors the new-article.php script to use the Auth class isLoggedIn method

--- new-article.php
+++ new-article.php
@@ -2,11 +2,11 @@
 require 'classes/Database.php';
 require 'classes/Article.php';
 require 'includes/url.php';
-require 'includes/auth.php';
+require 'classes/Auth.php';
 
 session_start();
 
-if (!isLoggedin()) {
+if (!Auth::isLoggedin()) {
   die('unauthorised');
 }
 
