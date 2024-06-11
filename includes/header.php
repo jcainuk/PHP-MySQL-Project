7a843242ce03875b16ecd6fe3cf717360722f4d3
Jonathan Cain <jcainuk@gmail.com>
2024-06-11 16:46:58 +0100

    adds site wide navigation links

--- includes/header.php
+++ includes/header.php
@@ -14,10 +14,12 @@
 
   <nav>
     <ul>
+      <li><a href="/cms/">Home</a></li>
       <?php if (Auth::isLoggedIn()) : ?>
-        <li><a href="logout.php">Log out</a></li>
+        <li><a href="/cms/admin/">Admin</a></li>
+        <li><a href="/cms/logout.php">Log out</a></li>
       <?php else : ?>
-        <li><a href="login.php">Log in</a></li>
+        <li><a href="/cms/login.php">Log in</a></li>
       <?php endif; ?>
     </ul>
   </nav>
