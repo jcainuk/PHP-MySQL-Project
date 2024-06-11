8119d820aba2d21ae34e8f118f9e7bd0380525eb
Jonathan Cain <jcainuk@gmail.com>
2024-06-11 16:30:11 +0100

    Displays the admin list of articles in a table and restricts access to the page

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,8 @@
 
 require '../includes/init.php';
 
+Auth::requireLogin();
+
 $conn = require '../includes/db.php';
 
 $articles = Article::getAll($conn);
@@ -21,24 +23,25 @@ $articles = Article::getAll($conn);
 
 <?php endif; ?>
 
-
+<h2>Administration</h2>
 
 <?php if (empty($articles)) : ?>
   <p>No articles found.</p>
 <?php else : ?>
-  <ul>
-    <?php foreach ($articles as $article) : ?>
-      <li>
-        <article>
-          <h2>
+  <table>
+    <thead>
+      <th>Title</th>
+    </thead>
+    <tbody>
+      <?php foreach ($articles as $article) : ?>
+        <tr>
+          <td>
             <a href="article.php?id=<?= $article['id'] ?>"><?= htmlspecialchars($article['title']); ?></a>
-          </h2>
-
-          <p><?= htmlspecialchars($article['content']); ?></p>
-        </article>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 <?php endif; ?>
 
 <?php require '../includes/footer.php' ?>
