b011b98c1ee4e648f53670e8fcd0401d206b6d39
Jonathan Cain <jcainuk@gmail.com>
2024-07-09 19:29:11 +0100

    adds script.js file and links it

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 </main>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous" defer></script>
+
+<script src="/cms/js/Script.js"></script>
 </body>
 
 </html>
